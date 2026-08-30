4cc3a947ee4b02d39651baac4192d6d88a59169a
jay timbol <jlc.timbol@gmail.com>
2026-08-29 21:37:49 -0700

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        perms = []
+
+        def backtrack(candidates, target, current, perms):
+            if len(current) >= target:
+                print(current)
+                perms.append(current[:])
+                return
+            for cand in candidates:
+                if cand not in current:
+                    current.append(cand)
+                    backtrack(candidates, target, current, perms)
+                    current.remove(cand)
+            return
+
+        
+
+        backtrack(nums, len(nums), [], perms)
+
+        return perms
+        
